224d4559c4de0feccebadd970d20e79040416c90
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-08-23 13:20:37 +0600

    Added bulma and main page
    - [ADDED] Bulma
    - [ADDED] Main page
    - [REMOVED] Comments from config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="hero is-bold is-danger is-fullheight">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title is-1">
+        АсАбАлАр
+      </h1>
+      <p class="subtitle is-3">
+        Театр, который мы заслужили
+      </p>
+          <div class="columns">
+            <div class="column is-offset-3 is-3 has-text-right">
+                <h2 class="title is-4">Мы в соцсетях</h2>
+                <ul>
+                    <li><a href="https://instagram.com/asabalar.theatre">Инстаграм</li>
+                    <li><a href="https://tiktok.com/asabalar">ТикТок</li>
+                    <li><a href="https://facebook.com/asabalar">Фейсбук</li>
+                    <li><a href="https://vk.com/asabalar">ВКонтакте</li>
+                </ul>
+            </div>
+            <div class="column is-3 has-text-left">
+                <h2 class="title is-4">Контакты</h2>
+                <ul>
+                    <li>
+                        <a href="mailto:bas@asabalar.kz">Почта</a>
+                    </li>
+                    <li>
+                        <a href="tel:+77004182565">Телефон</a>
+                    </li>
+                </ul>
+            </div>
+          </div>
+    </div>
+  </div>
+</section>
